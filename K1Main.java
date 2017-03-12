e7ddfe6da6ba7ce714621d7bec4252e55a58ab4c
srg <sergiomandelli77@gmail.com>
2017-03-12 11:39:19 +0100

    Main modifications

--- K1Main.java
+++ K1Main.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 
 public class K1Main {
+
+
    public static void main(String[] args) {
 
       // READS FILE TO AN ARRAYLIST 
@@ -11,12 +13,19 @@ public class K1Main {
       // Instance to get the interactions 
       K1Iterator iterator = new K1Iterator(list);
 
-      // CREATES A FRAME WITH THE FIRST QUESTION
-      K1JFrame k1JFrame = new K1JFrame(iterator);
-      k1JFrame.setDefaultCloseOperation(K1JFrame.EXIT_ON_CLOSE);
-      k1JFrame.setSize(600,400);
-      k1JFrame.setExtendedState(K1JFrame.MAXIMIZED_BOTH); 
-      k1JFrame.setVisible(true);
+      // Build the JFrame with the K1Iterator to process the interactions 
+      buildFrame(iterator) ;
 
    } // main end
+
+
+   // Method to build the main program JFrame
+   static void buildFrame(K1Iterator iterator) {
+       K1JFrame k1JFrame = new K1JFrame(iterator);
+       k1JFrame.setDefaultCloseOperation(K1JFrame.EXIT_ON_CLOSE);
+       k1JFrame.setSize(600,400);
+       k1JFrame.setExtendedState(K1JFrame.MAXIMIZED_BOTH); 
+       k1JFrame.setVisible(true);
+   }
+
 }// Class end
