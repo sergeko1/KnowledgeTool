e31ce505f405e9b6cc845b29c15081cdac7db792
srg <sergiomandelli77@gmail.com>
2017-03-12 11:55:44 +0100

    Starting to update the style

--- K1Main.java
+++ K1Main.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList;
 
-public class K1Main {
-
-
-   public static void main(String[] args) {
-
+public class K1Main 
+{
+   public static void main(String[] args) 
+   {
       // READS FILE TO AN ARRAYLIST 
       K1Reader reader = new K1Reader();
       reader.openFile();
@@ -15,17 +14,16 @@ public class K1Main {
 
       // Build the JFrame with the K1Iterator to process the interactions 
       buildFrame(iterator) ;
-
    } // main end
 
-
    // Method to build the main program JFrame
-   static void buildFrame(K1Iterator iterator) {
+   static void buildFrame(K1Iterator iterator) 
+   {
        K1JFrame k1JFrame = new K1JFrame(iterator);
        k1JFrame.setDefaultCloseOperation(K1JFrame.EXIT_ON_CLOSE);
        k1JFrame.setSize(600,400);
        k1JFrame.setExtendedState(K1JFrame.MAXIMIZED_BOTH); 
        k1JFrame.setVisible(true);
-   }
+   } // end of buildFrame method
 
 }// Class end
